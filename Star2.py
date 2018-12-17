026ef4a2d7f89ab6c2fa21d601a5be4fbe0c64a9
Jddanan <jddanan@gmail.com>
2018-12-17 17:09:53 +0200

    first commit

---
+++ Star2.py
@@ -0,0 +1,30 @@
+import feedparser
+import json
+from random import randint
+from bottle import route, run, static_file
+
+feed = feedparser.parse('https://www.dailymail.co.uk/articles.rss')
+
+
+@route('/')
+def app():
+    return static_file("Star2.html", root="")
+
+
+@route('/Article')
+def headline():
+    headlines = []
+    r = randint(0, len(feed["entries"]))
+    for i in range(r, r + 10):
+        title = feed["entries"][i]["title"]
+        link = "<a href=" + feed["entries"][i]["link"] + ">Go to the article</a>"
+        headlines.append({"title": title, "link": link})
+    return json.dumps(headlines)
+
+
+def main():
+    run(host='localhost', port=7000)
+
+
+if __name__ == '__main__':
+    main()
